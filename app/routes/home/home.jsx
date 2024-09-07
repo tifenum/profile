206aa37d9ab9225f2dbb0cf5347f336bd8236057
Hbib Boukadida <boukadidahbib@gmail.com>
2024-09-07 23:32:13 +0100

    one day of work

---
+++ app/routes/home/home.jsx
@@ -0,0 +1,170 @@
+import gamestackTexture2Large from '~/assets/310438812_1630051494181292_6338321910946070443_n.jpg';
+import gamestackTexture2Placeholder from '~/assets/310438812_1630051494181292_6338321910946070443_n.jpg'; 
+import gamestackTexture2 from '~/assets/310438812_1630051494181292_6338321910946070443_n.jpg';
+import gamestackTextureLarge from '~/assets/310612999_1630051694181272_894566700937356929_n.jpg';
+import gamestackTexturePlaceholder from '~/assets/310612999_1630051694181272_894566700937356929_n.jpg';
+import gamestackTexture from '~/assets/310612999_1630051694181272_894566700937356929_n.jpg';
+import sliceTextureLarge from '~/assets/1716058500964.jpg';
+import sliceTexturePlaceholder from '~/assets/1716058500964.jpg';
+import sliceTexture from '~/assets/1716058500964.jpg';
+import sprTextureLarge from '~/assets/Screenshot 2024-09-07 194431.png';
+import sprTexturePlaceholder from '~/assets/Screenshot 2024-09-07 194431.png';
+import sprTexture from '~/assets/Screenshot 2024-09-07 194431.png';
+import { Footer } from '~/components/footer';
+import { baseMeta } from '~/utils/meta';
+import { Intro } from './intro';
+import { Profile } from './profile';
+import { ProjectSummary } from './project-summary';
+import { useEffect, useRef, useState } from 'react';
+import config from '~/config.json';
+import styles from './home.module.css';
+
+// Prefetch draco decoader wasm
+export const links = () => {
+  return [
+    {
+      rel: 'prefetch',
+      href: '/draco/draco_wasm_wrapper.js',
+      as: 'script',
+      type: 'text/javascript',
+      importance: 'low',
+    },
+    {
+      rel: 'prefetch',
+      href: '/draco/draco_decoder.wasm',
+      as: 'fetch',
+      type: 'application/wasm',
+      importance: 'low',
+    },
+  ];
+};
+
+export const meta = () => {
+  return baseMeta({
+    title: 'Designer + Developer',
+    description: `Design portfolio of ${config.name} — a product designer working on web & mobile apps with a focus on motion, experience design, and accessibility.`,
+  });
+};
+
+export const Home = () => {
+  const [visibleSections, setVisibleSections] = useState([]);
+  const [scrollIndicatorHidden, setScrollIndicatorHidden] = useState(false);
+  const intro = useRef();
+  const projectOne = useRef();
+  const projectTwo = useRef();
+  const projectThree = useRef();
+  const details = useRef();
+
+  useEffect(() => {
+    const sections = [intro, projectOne, projectTwo, projectThree, details];
+
+    const sectionObserver = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const section = entry.target;
+            observer.unobserve(section);
+            if (visibleSections.includes(section)) return;
+            setVisibleSections(prevSections => [...prevSections, section]);
+          }
+        });
+      },
+      { rootMargin: '0px 0px -10% 0px', threshold: 0.1 }
+    );
+
+    const indicatorObserver = new IntersectionObserver(
+      ([entry]) => {
+        setScrollIndicatorHidden(!entry.isIntersecting);
+      },
+      { rootMargin: '-100% 0px 0px 0px' }
+    );
+
+    sections.forEach(section => {
+      sectionObserver.observe(section.current);
+    });
+
+    indicatorObserver.observe(intro.current);
+
+    return () => {
+      sectionObserver.disconnect();
+      indicatorObserver.disconnect();
+    };
+  }, [visibleSections]);
+
+  return (
+    <div className={styles.home}>
+      <Intro
+        id="intro"
+        sectionRef={intro}
+        scrollIndicatorHidden={scrollIndicatorHidden}
+      />
+      <ProjectSummary
+        id="project-1"
+        sectionRef={projectOne}
+        visible={visibleSections.includes(projectOne.current)}
+        index={1}
+        title="Founder and ex-president of Vanguard Chess Club"
+        description=" Vanguard Chess Club, the first chess club established at ENSIT. Through this initiative, I brought together students who share a passion for strategy, intellect, and camaraderie."
+
+        model={{
+          type: 'laptop',
+          alt: 'Smart Sparrow lesson builder',
+          textures: [
+            {
+              srcSet: `${sprTexture} 1280w, ${sprTextureLarge} 2560w`,
+              placeholder: sprTexturePlaceholder,
+            },
+          ],
+        }}
+      />
+      <ProjectSummary
+        id="project-2"
+        alternate
+        sectionRef={projectTwo}
+        visible={visibleSections.includes(projectTwo.current)}
+        index={2}
+        title="Active Member of Tunis Sports University"
+        description="Design and development for a video game tracking app built in React Native"
+
+        model={{
+          type: 'phone',
+          alt: 'App login screen',
+          textures: [
+            {
+              srcSet: `${gamestackTexture} 375w, ${gamestackTextureLarge} 750w`,
+              placeholder: gamestackTexturePlaceholder,
+            },
+            {
+              srcSet: `${gamestackTexture2} 375w, ${gamestackTexture2Large} 750w`,
+              placeholder: gamestackTexture2Placeholder,
+            },
+          ],
+        }}
+      />
+      <ProjectSummary
+        id="project-3"
+        sectionRef={projectThree}
+        visible={visibleSections.includes(projectThree.current)}
+        index={3}
+        title="Passionate about problem solving and thriving in hackathon challenges."
+        description="Passionate about solving problems and thriving in hackathon challenges."
+        model={{
+          type: 'laptop',
+          alt: 'Annotating a biomedical image in the Slice app',
+          textures: [
+            {
+              srcSet: `${sliceTexture} 800w, ${sliceTextureLarge} 1920w`,
+              placeholder: sliceTexturePlaceholder,
+            },
+          ],
+        }}
+      />
+      <Profile
+        sectionRef={details}
+        visible={visibleSections.includes(details.current)}
+        id="details"
+      />
+      <Footer />
+    </div>
+  );
+};
